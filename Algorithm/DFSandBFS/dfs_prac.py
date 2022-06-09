40e8f0516b9fa2d830c9d054ce9683d3da563bee
yoolim12 <yoolim12@hanmail.net>
2022-06-09 20:30:49 +0900

    dfs practice

---
+++ Algorithm/DFSandBFS/dfs_prac.py
@@ -0,0 +1,12 @@
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+
+    for el in graph[v]:
+        if not visited[el]:
+            dfs(graph, el, visited)
+
+graph = [[], [2,3,8], [1,7], [1,4,5], [3,5], [3,4], [7], [2,6,8], [1,7]]
+visited = [False] * len(graph)
+
+dfs(graph, 1, visited)
