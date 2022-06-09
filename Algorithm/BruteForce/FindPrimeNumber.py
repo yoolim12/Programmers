5aa5f094522cc32fa1fd15ec2682dd21e2d48028
yoolim12 <yoolim12@hanmail.net>
2022-06-09 20:29:22 +0900

    완전탐색

--- Algorithm/BruteForce/FindPrimeNumber.py
+++ Algorithm/BruteForce/FindPrimeNumber.py
@@ -1,4 +1,7 @@
 # 소수 찾기
+# 파이썬 소수 개수 구하기 https://wikidocs.net/21638
+# https://velog.io/@front/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%B9%B4%ED%8E%AB
+# 순열로 나온 결과 문자열 형태로 이어 https://programmers.co.kr/learn/courses/4008/lessons/12836
 import math
 import itertools
 
