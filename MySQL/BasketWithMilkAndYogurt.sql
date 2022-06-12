3939c81a7a9c5f565b531741f9bf3ddfff7b84b7
yoolim12 <yoolim12@hanmail.net>
2022-06-12 15:01:05 +0900

    with를 이용한 또 다른 풀이

--- MySQL/BasketWithMilkAndYogurt.sql
+++ MySQL/BasketWithMilkAndYogurt.sql
@@ -10,6 +10,26 @@ cart_id in
 group by cart_id
 order by cart_id
 
+-- 또 다른 나의 풀이
+-- with t1 as (
+--     select cart_id
+--     from cart_products
+--     where name = 'Milk'
+-- ), t2 as (
+--     select cart_id
+--     from cart_products
+--     where name = 'Yogurt'
+-- )
+
+
+
+
+-- select t1.cart_id
+-- from t1
+-- inner join t2
+-- on t1.cart_id = t2.cart_id
+-- order by cart_id;
+
 -- 다른 좋은 풀이
 -- SELECT DISTINCT A.CART_ID
 -- FROM CART_PRODUCTS AS A, CART_PRODUCTS AS B
